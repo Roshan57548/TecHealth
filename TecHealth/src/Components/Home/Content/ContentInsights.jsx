3a375b33a7d5d269d0e003d1bdbbad31802db742
kuldeep <kuldeepkumar58426@gmail.com>
2023-06-01 00:38:16 +0530

    Modified Insight Component

--- TecHealth/src/Components/Home/Content/ContentInsights.jsx
+++ TecHealth/src/Components/Home/Content/ContentInsights.jsx
@@ -1,26 +1,26 @@
 const ContentInsights = {
-    Heading: "Insights",
-    Subheading: [
-      { Subheading1: "Response Time :" },
-      { Subheading2: " Less than 15 Minutes" },
-    ],
-    ProvidedService: [
-      {
-        Number1: "100k+",
-        Description1: "Lives Saved",
-      },
+  Heading: "Insights",
+  Subheading: [
+    { Subheading1: "Response Time :" },
+    { Subheading2: " Less than 15 Minutes" },
+  ],
+  ProvidedService: [
+    {
+      Number1: "100k+",
+      Description1: "Lives Saved",
+    },
+
+    {
+      Number2: "800k+",
+      Description2: "Patients Served",
+    },
+
+    {
+      Number3: "500+",
+      Description3: "Ambulance",
+    },
+  ],
+};
   
-      {
-        Number2: "800k+",
-        Description2: "Patients Served",
-      },
+export default ContentInsights;
   
-      {
-        Number3: "500+",
-        Description3: "Ambulance Providers",
-      },
-    ],
-  };
-    
-  export default ContentInsights;
-    
