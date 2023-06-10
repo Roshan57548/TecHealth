5e77669a6a9630c38dde223faf5e51f474b519c7
kuldeep <kuldeepkumar58426@gmail.com>
2023-06-10 19:59:37 +0530

    Added BlogHeader Component

--- TecHealth/src/Components/Blog/Components/BlogHeader.jsx
+++ TecHealth/src/Components/Blog/Components/BlogHeader.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./Style/BlogHeader.css";
 
-const BlogHeader = () => {
+const BlogHeader = (props) => {
   return (
-    <div>Heading</div>
+    <div className="Blog_0">
+      <div className="Blog_Content">
+        <h3>
+          <span className="Blog_Heading1_Span">{props.data.heading_Data_One}</span>
+          <span className="Blog_Heading2_Span">{props.data.heading_Data_Two}</span>
+        </h3>
+        <p>
+          {props.data.paragraph}
+        </p>
+      </div>
+    </div>
   )
 }
 
--- TecHealth/src/Components/Blog/Content/ContentBlogHeader.jsx
+++ TecHealth/src/Components/Blog/Content/ContentBlogHeader.jsx
@@ -1,5 +1,9 @@
 const ContentBlogHeader = {
-    
+  heading_Data_One: "My Latest ",
+  heading_Data_Two: "blog",
+  paragraph:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur magni, omnis nemo distinctio porro fugiat?",
+
   };
   
   export default ContentBlogHeader;
