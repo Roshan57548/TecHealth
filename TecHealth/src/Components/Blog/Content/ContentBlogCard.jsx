0826a0f25cbd4b3a0016da3524a33ea1c73f9562
roshan.prajapati <roshanprajapati678@gmail.com>
2023-06-18 16:21:15 +0530

    done

--- TecHealth/src/Components/Blog/Content/ContentBlogCard.jsx
+++ TecHealth/src/Components/Blog/Content/ContentBlogCard.jsx
@@ -6,7 +6,7 @@ const ContentBlogCard = {
         "https://images.unsplash.com/photo-1485848395967-65dff62dc35b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80",
       mainHeading: "Bottom Line of Dietary Supplement",
       mainContent:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry like Aldus PageMaker including versions of Lorem  Aldus PageMaker including versions of Lorem   ",
+        "Lorem  Aldus PageMaker including versions of Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry like Aldus PageMaker including versions of Lorem  Aldus PageMaker including versions of Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry like Aldus PageMaker including versions of Lorem  Aldus PageMaker including versions of Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry like Aldus PageMaker including versions of Lorem  Aldus PageMaker including versions of Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry like Aldus PageMaker including versions of Lorem  Aldus PageMaker including versions of Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry like",
       content: [
         {
           subHeading: "Successful Family Planning Programs",
