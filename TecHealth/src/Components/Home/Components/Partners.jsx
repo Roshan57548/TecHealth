bc60646ca11afac539bde429cfbaf3f3b5ad8622
Chandan Kumar <krc6893@gmail.com>
2023-07-04 14:17:28 +0530

    Link imported

--- TecHealth/src/Components/Home/Components/Partners.jsx
+++ TecHealth/src/Components/Home/Components/Partners.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 import Form from "../../Form/Amb_Booking_Form";
 import "./Style/Partners.css";
 const Partners = (props) => {
